33ebef19e99e579a555fc36ad3659d305a9f666c
Yamuna_M <yamuna1837@gmail.com>
2024-07-30 11:40:29 +0530

    Completed UI

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import Task from './components/MyTasks';
+import './App.css';
+
+const sampledata = [
+  {
+    "id": 1,
+    "title": "Task 1",
+    "description": "Description for Task 1",
+    "completed": false,
+    "timestamp": "2024-07-29T10:00:00Z"
+  },
+  {
+    "id": 2,
+    "title": "Task 2",
+    "description": "Description for Task 2",
+    "completed": false,
+    "timestamp": "2024-07-28T14:00:00Z"
+  }
+]
+
+function App() {
+  const [tasks, setTasks] = useState(sampledata);
+  const [search, setSearch] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskDescription, setNewTaskDescription] = useState('');
+
+  useEffect(() => {
+    fetch('../tasks.json')
+      .then(response => response.json())
+      .then(data => setTasks(data));
+  }, []);
+
+
+
+  const handleCreateTask = () => {
+    
+
+    const newTask = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      description: newTaskDescription,
+      completed: false,
+      timestamp: new Date().toISOString(),
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
+    setNewTaskDescription('');
+  };
+
+  const handleToggleComplete = (id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+  };
+
+  const handleUpdateTask = (id, newTitle, newDescription) => {
+    setTasks(tasks.map(task =>
+      task.id === id
+        ? { ...task, title: newTitle, description: newDescription, timestamp: new Date().toISOString() }
+        : task
+    ));
+  };
+
+  const filteredTasks = tasks.filter(task =>
+    task.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleDeleteTask = (id) => {
+    if (window.confirm('Are you sure you want to delete this task?')) {
+      setTasks(tasks.filter(task => task.id !== id));
+    }
+  };
+  return (
+    <div className="container" >
+      <h1 className='mainheading' style={{ fontSize: 30 }}>To-Do List</h1>
+
+      <div className="container2" style={{ height: '100%' }}>
+        <input
+          className='commontextinpustyle'
+          type="text"
+          placeholder="Search tasks..."
+          value={search}
+        />
+        <h2 className='mainheading'>Create Task</h2>
+        <input
+          className='commontextinpustyle'
+          type="text"
+          placeholder="Title"
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+
+        />
+        <textarea
+          name="postContent"
+          rows={4}
+          cols={20}
+          placeholder="Description"
+          value={newTaskDescription}
+          onChange={(e) => setNewTaskDescription(e.target.value)}
+        />
+
+
+        <div style={{ width: '100%',marginTop:10, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+
+          <button style={{ width: '30%' }} className='ButtomStyle' onClick={handleCreateTask}>Add Task</button>
+        </div>
+
+
+        <ul>
+          {filteredTasks.map(task => (
+            <Task
+              key={task.id}
+              task={task}
+              onToggleComplete={handleToggleComplete}
+              onUpdateTask={handleUpdateTask}
+              onDeleteTask={handleDeleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyTasks.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+const Task = ({ task, onToggleComplete, onUpdateTask, onDeleteTask }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [title, setTitle] = useState(task.title);
+    const [description, setDescription] = useState(task.description);
+
+    const handleExpandToggle = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const handleUpdateClick = () => {
+        if (title.trim() === '' || description.trim() === '') {
+            alert('Both title and description are required.');
+            return;
+        }
+        onUpdateTask(task.id, title, description);
+        setIsEditing(false);
+    };
+
+    return (
+        <li >
+            <div >
+
+                <div style={{ display: 'flex' }}>
+                    <input
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => onToggleComplete(task.id)}
+                    />
+                    <div onClick={handleExpandToggle} style={{cursor:'pointer' ,flex: 1, display: 'flex', justifyContent: 'space-between' }}>
+                        <span >
+                            {task.title}
+                        </span>
+                        <span >
+                            <i className={`expand-icon fas ${isExpanded ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+                        </span>
+                    </div>
+                </div>
+
+                {isExpanded && (
+                    <div  >
+                        {isEditing ? (
+                            
+                            <div  className="editcontainer" >
+                                <div style={{width:'100%',display:'flex',flexDirection:'column',alignItems:'center'}}>
+                                <input
+                                    className='editInputinput'
+                                    type="text"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+                                <textarea
+                                    className='editInputinput'
+                                    name="postContent"
+                                    rows={4}
+                                    cols={40}
+                                    placeholder="Description"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                />
+                            </div>
+
+                            <div style={{width:'90%',display:'flex',justifyContent:'flex-end'}}>
+                                <button className='ButtomStyle' onClick={handleUpdateClick}>Save</button>
+                                <button className='ButtomStyle' onClick={() => setIsEditing(false)}>Cancel</button>
+                            </div>
+
+                            </div>
+                        ) : (
+                            <div > 
+                                <p className=''>{task.description}</p>
+                                <p>Last Updated: {new Date(task.timestamp).toLocaleString()}</p>
+                                <button className='ButtomStyle' onClick={() => setIsEditing(true)}>Edit</button>
+                                <button className='ButtomStyle' onClick={() => onDeleteTask(task.id)}>Delete</button>
+                            </div>
+                        )}
+                    </div>
+                )}
+
+            </div>
+        </li>
+    );
+};
+
+export default Task;
